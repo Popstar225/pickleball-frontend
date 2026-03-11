c4652fbfd8ca19a21aa1851faa984c4409899233
Popstar225 <popdev225@gmail.com>
2026-03-11 21:58:59 +0200

    feat: prepare production

--- src/lib/const.ts
+++ src/lib/const.ts
@@ -1,6 +1,6 @@
-const API_URI = import.meta.env.VITE_API_URI || 'http://localhost:5000';
+const API_URI = import.meta.env.VITE_API_URI || 'https://pickleball-backend-omega.vercel.app';
 
 export const baseURL = `${API_URI}/api/v1`;
 export const imageBaseURL = API_URI;
 
-export const siteBaseURL = import.meta.env.VITE_BASIC_URI || 'http://localhost:8080';
+export const siteBaseURL = import.meta.env.VITE_BASIC_URI || 'https://pickleball-frontend-ten.vercel.app';
